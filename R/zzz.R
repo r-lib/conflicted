41001bc34bf1d40f043e22b981f0a6894dc1c129
hadley <h.wickham@gmail.com>
2017-05-24 10:27:57 -0700

    Export strict_activate and strict_deactivate

--- R/zzz.R
+++ R/zzz.R
@@ -9,11 +9,19 @@
 }
 
 
+#' Manually activate and deactive strict mode
+#'
+#' For experts only.
+#'
+#' @keywords internal
+#' @export
 strict_deactivate <- function() {
   remove_shims()
   remove_conflicts()
 }
 
+#' @export
+#' @rdname strict_deactivate
 strict_activate <- function() {
   register_shims()
   register_conflicts()
