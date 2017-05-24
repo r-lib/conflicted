1b0c283b5676165be3d49b3cad85df3c52babaec
hadley <h.wickham@gmail.com>
2017-05-24 10:12:45 -0700

    Detect if in strict mode
    Fixes #2

--- R/strict.R
+++ R/strict.R
@@ -1,2 +1,10 @@
 #' @import rlang
 NULL
+
+
+#' Is code run in a strict environment?
+#'
+#' @export
+is_strict <- function() {
+  "strict_conflicts" %in% search() && "strict_shims" %in% search()
+}
