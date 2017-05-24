7899a7e218259d876057ff6f3c312aff3237552b
hadley <h.wickham@gmail.com>
2017-05-24 05:45:27 -0700

    Inform about shims

--- R/shims.R
+++ R/shims.R
@@ -5,6 +5,10 @@ register_shims <- function() {
 
   strict_shims <- attach(new_environment(), name = "strict_shims")
   register_shim_T_F(strict_shims)
+
+  # What's getting overriden
+  shims <- ls(strict_shims)
+  message("Adding stict shims for: ", paste(shims, collapse = ", "))
 }
 
 register_shim_T_F <- function(env) {
