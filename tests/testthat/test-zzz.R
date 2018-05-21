6f0fe8bceb0c624721de24384030a2fdc079846c
hadley <h.wickham@gmail.com>
2018-05-20 19:27:48 -0500

    Test removal on detach

---
+++ tests/testthat/test-zzz.R
@@ -0,0 +1,9 @@
+context("test-zzz.R")
+
+test_that("detaching removes shims", {
+  # I honestly don't understand why this works - detaching the package
+  # being tested seems like a bad bad idea; maybe it's ok because it's the
+  # last test?
+  detach(package:conflicted)
+  testthat::expect_false("conflicted" %in% search())
+})
