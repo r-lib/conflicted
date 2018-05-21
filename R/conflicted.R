f493bcd1ce683a39840371dcd2432c30e0a76502
hadley <h.wickham@gmail.com>
2018-05-21 13:26:07 -0500

    Document how to resolve conflict persistently

--- R/conflicted.R
+++ R/conflicted.R
@@ -1,3 +1,14 @@
+#' @section Resolving conflicts:
+#'
+#' To permanently resolve a conflict within a session, use assignment:
+#'
+#' \preformatted{
+#' library(conflicted)
+#' library(dplyr)
+#'
+#' filter <- dplyr::filter
+#' }
+#'
 #' @keywords internal
 #' @import rlang
 "_PACKAGE"
