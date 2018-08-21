443a5ae4f5ed6cd83e11bc4e70ed1bd504ef3119
hadley <h.wickham@gmail.com>
2018-08-21 12:23:49 -0500

    Only detach if not loaded by devtools

--- R/zzz.R
+++ R/zzz.R
@@ -3,5 +3,9 @@
 }
 
 .onDetach <- function(...) {
+  # detaching when loaded by devtools seems to detach tools:rstudio too :(
+  if (pkg_devtools("conflicted"))
+    return()
+
   conflicts_reset()
 }
--- tests/testthat/test-zzz.R
+++ tests/testthat/test-zzz.R
@@ -1,6 +1,8 @@
 context("test-zzz.R")
 
 test_that("detaching removes shims", {
+  skip_if(pkg_devtools("conflicted"))
+
   # I honestly don't understand why this works - detaching the package
   # being tested seems like a bad bad idea; maybe it's ok because it's the
   # last test?
