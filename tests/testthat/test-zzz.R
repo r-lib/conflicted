779842e4e8c6fadfa2fa7d0ef219dbbbc298fd5a
Hadley Wickham <h.wickham@gmail.com>
2021-11-26 09:50:35 -0600

    Perform nasty test in separate process

--- tests/testthat/test-zzz.R
+++ tests/testthat/test-zzz.R
@@ -1,8 +1,9 @@
 test_that("detaching removes shims", {
-  # I honestly don't understand why this works - detaching the package
-  # being tested seems like a bad bad idea; maybe it's ok because it's the
-  # last test?
-  testthat::expect_true(conflicts_attached())
-  detach(package:conflicted)
-  testthat::expect_false(".conflicts" %in% search())
+  # Do in separate process to avoid interferring with tests
+  in_search <- callr::r(function() {
+    library(conflicted)
+    detach("package:conflicted")
+    ".conflicts" %in% search()
+  })
+  expect_false(in_search)
 })
